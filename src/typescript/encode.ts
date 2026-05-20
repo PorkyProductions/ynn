3c45f7a5c552f28dac9ca4ecb50567c770ab3063
Ryan Mullin <82683251+hiteacheryouare@users.noreply.github.com>
2026-05-20 11:54:27 -0400

    update readme links

--- src/typescript/encode.ts
+++ src/typescript/encode.ts
@@ -1,14 +1,5 @@
-import { encode } from 'js-base64';
+import { encodeURI } from 'js-base64';
 import { URLArticleProps } from './types';
 
-export const encodeData = (data: URLArticleProps) => {
-	let encodedString = encode(JSON.stringify(data));
-	while (encodedString.endsWith('=')) {
-		encodedString = encodedString.slice(0, -1);
-	}
-	while (encodedString.includes("/")) {
-		encodedString = encodedString.replace("/", "");
-	}
-	return encodedString;
-};
+export const encodeData = (data: URLArticleProps) => encodeURI(JSON.stringify(data));
 export default encodeData;
